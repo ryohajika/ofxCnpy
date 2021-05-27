6dc3c3d4b6550e0bd0fedb162ade72d1a9ec6e5b
Ryo Hajika <ryo@imaginaryshort.com>
2021-05-27 21:24:45 +1200

    Npy export updated

--- src/ofxCnpy.h
+++ src/ofxCnpy.h
@@ -2,7 +2,9 @@
 
 #include <unordered_map>
 #include "cnpy.h"
+#include "ofLog.h"
 #include "ofFileUtils.h"
+#include "ofUtils.h"
 
 enum CNPY_FILE_TYPE {
 	CNPY_NPY = 0,
@@ -12,83 +14,110 @@ enum CNPY_FILE_TYPE {
 namespace ofxCnpy {
 
 struct DirectoryDealer {
+	std::string _export_dir_full;
+	std::string _export_file_name;
+	std::string _export_file_ext;
+	
 	bool setExportDirectory(std::string full_dir_path){
+		if(full_dir_path == ""){
+			_export_dir_full = ofFilePath::getAbsolutePath("");
+		}else{
+			if(!ofDirectory::doesDirectoryExist(full_dir_path)){
+				bool _b = ofDirectory::createDirectory(full_dir_path, false, true);
+				if(!_b){
+					ofLogError("ofxCnpy::DirectoryDealer",
+							   "got an error during creating directory(ies)");
+					return false;
+				}
+			}
+		}
 		
+		if(ofDirectory::doesDirectoryExist(full_dir_path)){
+			ofLogNotice("ofxCnpy::DirectoryDealer",
+						"directory set to: " + full_dir_path);
+			_export_dir_full = full_dir_path;
+			return true;
+		}else{
+			ofLogError("ofxCnpy::DirectoryDealer",
+					   "came across with some issue while setting exporting directory");
+			return false;
+		}
+	}
+	inline std::string getExportDirectoryFullPath(){
+		return _export_dir_full;
+	}
+	
+	bool setExportFilename(std::string file_name,
+						   std::string extension){
+		if(extension.find(".") != std::string::npos){
+			_export_file_ext = extension.substr(extension.find(".")+1);
+		}else{
+			_export_file_ext = extension;
+		}
+		
+		if(file_name.find(_export_file_ext) != std::string::npos){
+			_export_file_name = file_name.substr(0, file_name.find(_export_file_ext));
+		}else{
+			_export_file_name = file_name;
+		}
+		
+		if(ofFile::doesFileExist(_export_dir_full + "/" + _export_file_name + "." + _export_file_ext, false)){
+			ofLogError("ofxCnpy::DirectoryDealer",
+					   "there's a file in same file name under the specified directory");
+			_export_file_name += "_" + std::to_string(ofGetUnixTime());
+			ofLogNotice("ofxCnpy::DirectoryDealer",
+						"new file name is: " + _export_file_name + "." + _export_file_ext);
+		}
+		
+		ofLogNotice("ofxCnpy::DirectoryDealer",
+					"Full path set as: " + _export_dir_full + "/" + _export_file_name + "." + _export_file_ext);
+		return true;
+	}
+	inline std::string getExportFileNameFull(){
+		return _export_dir_full + "/" + _export_file_name + "." + _export_file_ext;
+	}
+	inline std::string getExportFileNameOnly(){
+		return _export_file_name;
+	}
+	inline std::string getExportFileExtensionOnly(){
+		return _export_file_ext;
 	}
 };
 
 struct NpyExporter {
 	DirectoryDealer dd_;
+	const std::string kNpyExt = "npy";
+	
+	inline bool setExportDir(std::string path){
+		return dd_.setExportDirectory(path);
+	}
+	
+	template<class T>
+	bool saveToNpy(std::vector<T> &data,
+				   std::vector<size_t> shape,
+				   std::string file_name,
+				   std::string export_path = ""){
+		// check if the directory exists first
+		if(!dd_.setExportDirectory(export_path)){
+			ofLogError("ofxCnpy::NpyExporter",
+					   "encountering directory generation issue so about the file saving");
+			return false;
+		}else{
+			if(file_name.find(kNpyExt) != std::string::npos){
+				dd_.setExportFilename(file_name.substr(0, 				  				  file_name.find(kNpyExt)),
+									  kNpyExt);
+			}else{
+				dd_.setExportFilename(file_name, kNpyExt);
+			}
+			cnpy::npy_save(dd_.getExportFileNameFull(), &data, shape, "w");
+			return true;
+		}
+	}
 };
 
 struct NpzExporter {
-	
+	DirectoryDealer dd_;
+	const std::string kNpzExt = "npz";
 };
 
 };	// namespace ofxCnpy
-
-class ofxCnpy {
-	public:
-		ofxCnpy(){
-			_data_dim.clear();
-			_export_path = ofFilePath::getAbsolutePath("");
-		}
-		~ofxCnpy(){
-			
-		}
-	
-		void init(std::vector<size_t> &shape,
-				  std::string full_dir_path,
-				  std::string file_name){
-			_export_path = full_dir_path;
-			this->init(shape, file_name);
-		}
-		
-		void init(std::vector<size_t> &shape,
-				  std::string file_name){
-			_b_initial_data = true;
-			_data_dim = shape;
-			
-			if(file_name.find_last_of(".npy") != std::string::npos){
-				_file_type = CNPY_NPY;
-			}
-			else if(file_name.find_last_of(".npz") != std::string::npos){
-				_file_type = CNPY_NPZ;
-			}else{
-				// error: file ext specified is not suitable
-			}
-		}
-		void setShape(std::vector<size_t> &shape){
-			_data_dim = shape;
-		}
-		inline std::vector<size_t>* getShape(){
-			return &_data_dim;
-		}
-	
-		CNPY_FILE_TYPE getFileType(){
-			
-		}
-		inline std::string getFileTypeExt(){
-			
-		}
-	
-		void setExportDir(std::string full_dir_path){
-			_export_path = full_dir_path;
-		}
-		inline std::string getExportDir(){
-			return _export_path;
-		}
-	
-	
-	private:
-		CNPY_FILE_TYPE _file_type;
-		const std::unordered_map<short int, std::string> CNPY_EXT_PAIR = {
-			{0, ".npy"},
-			{1, ".npz"}
-		};
-		std::vector<size_t> _data_dim;
-		bool _b_initial_data;
-
-		std::string _export_path;
-		std::string _export_name;
-};
